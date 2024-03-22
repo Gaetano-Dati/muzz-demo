a5b49fd14f8e251b5f0ec0cf59ad73a7961d844f
Gaetano Dati <datiga001@group.pirelli.com>
2024-03-22 19:00:55 +0100

    Improved Message class

--- app/src/main/java/com/garrodroideveloper/muzzexercise/storage/entities/Message.kt
+++ app/src/main/java/com/garrodroideveloper/muzzexercise/storage/entities/Message.kt
@@ -10,5 +10,5 @@ data class Message(
     @ColumnInfo(name = "created_at") val createdAt: Long,
     @ColumnInfo(name = "has_been_seen") val hasBeenSeen: Boolean,
     @ColumnInfo(name = "message") val message: String,
-    @ColumnInfo(name = "sent_by") val username: String
+    @ColumnInfo(name = "is_from_me") val isFromMe: Boolean,
 )
