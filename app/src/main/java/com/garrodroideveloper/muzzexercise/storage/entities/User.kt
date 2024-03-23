e4727eb427a432de7c747dc9cea2d24de3337673
Gaetano Dati <datiga001@group.pirelli.com>
2024-03-23 19:38:52 +0100

    Adding uid manually in User

--- app/src/main/java/com/garrodroideveloper/muzzexercise/storage/entities/User.kt
+++ app/src/main/java/com/garrodroideveloper/muzzexercise/storage/entities/User.kt
@@ -6,6 +6,6 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class User(
-    @PrimaryKey(autoGenerate = true) val uid: Int? = null,
+    @PrimaryKey val uid: Int,
     @ColumnInfo(name = "first_name") val firstName: String?,
 )
