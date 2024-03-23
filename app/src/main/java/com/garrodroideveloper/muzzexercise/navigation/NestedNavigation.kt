044c700a4da801a71a85044106b9827a7631a95c
Gaetano Dati <datiga001@group.pirelli.com>
2024-03-23 18:32:39 +0100

    Added click fun in NestedNavigation.kt

--- app/src/main/java/com/garrodroideveloper/muzzexercise/navigation/NestedNavigation.kt
+++ app/src/main/java/com/garrodroideveloper/muzzexercise/navigation/NestedNavigation.kt
@@ -1,16 +1,16 @@
 package com.garrodroideveloper.muzzexercise.navigation
 
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
 import com.garrodroideveloper.muzzexercise.home.MainScreen
 import com.garrodroideveloper.muzzexercise.welcome.WelcomeScreen
 
-fun NavGraphBuilder.addMain(navController: NavHostController) {
+fun NavGraphBuilder.addMain() {
     navigation(startDestination = MainNavItems.Welcome.route, route = MainNavItems.Start.route) {
         composable(MainNavItems.Welcome.route) {
-            WelcomeScreen()
+            WelcomeScreen { username ->
+            }
         }
         composable(MainNavItems.Main.route) {
             MainScreen()
