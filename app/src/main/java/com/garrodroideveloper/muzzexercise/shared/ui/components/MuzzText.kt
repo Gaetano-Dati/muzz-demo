40a658c35033af170dd6d1ec0f58c451faf80494
Gaetano Dati <datiga001@group.pirelli.com>
2024-03-23 15:25:04 +0100

    Improved MuzzText.kt

--- app/src/main/java/com/garrodroideveloper/muzzexercise/shared/ui/components/MuzzText.kt
+++ app/src/main/java/com/garrodroideveloper/muzzexercise/shared/ui/components/MuzzText.kt
@@ -4,27 +4,46 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun MuzzSmallText(
     modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colorScheme.onBackground,
     value: String,
 ) {
     Text(
         modifier = modifier,
         text = value,
         style = MaterialTheme.typography.bodySmall,
+        color = color,
     )
 }
 
 @Composable
 fun MuzzMediumText(
     modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colorScheme.onBackground,
     value: String,
 ) {
     Text(
         modifier = modifier,
         text = value,
         style = MaterialTheme.typography.bodyMedium,
+        color = color,
+    )
+}
+
+@Composable
+fun MuzzTitleText(
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colorScheme.onBackground,
+    value: String,
+) {
+    Text(
+        modifier = modifier,
+        text = value,
+        style = MaterialTheme.typography.titleLarge,
+        color = color,
     )
 }
