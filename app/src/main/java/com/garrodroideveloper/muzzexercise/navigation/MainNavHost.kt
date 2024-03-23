3547b244d43bf1572d0ce086e4f32c4e808ccf7c
Gaetano Dati <datiga001@group.pirelli.com>
2024-03-23 18:33:20 +0100

    Indented code in MainNavHost

--- app/src/main/java/com/garrodroideveloper/muzzexercise/navigation/MainNavHost.kt
+++ app/src/main/java/com/garrodroideveloper/muzzexercise/navigation/MainNavHost.kt
@@ -9,59 +9,55 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.garrodroideveloper.muzzexercise.R
-import com.garrodroideveloper.muzzexercise.shared.ui.components.MuzzMediumText
+import com.garrodroideveloper.muzzexercise.shared.ui.components.MuzzTitleText
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainNavHost() {
     val navController = rememberNavController()
     val navBackStackEntry by navController.currentBackStackEntryAsState()
-    val context = LocalContext.current
 
     val noBackButtonScreens =
         listOf(
             MainNavItems.Welcome,
         ).map { it.route }
 
-    val noBottomBarRoutes =
-        listOf(
-            MainNavItems.Welcome,
-        ).map { it.route }
-
     Scaffold(
         topBar = {
-            TopAppBar(
-                colors =
-                    topAppBarColors(
-                        containerColor = MaterialTheme.colorScheme.primaryContainer,
-                        titleContentColor = MaterialTheme.colorScheme.primary,
-                    ),
-                title = {
-                    MuzzMediumText(value = "Title")
-                },
-                navigationIcon = {
-                    if (navBackStackEntry?.destination?.route !in noBackButtonScreens) {
-                        // Show Back button
-                        IconButton(onClick = { navController.navigateUp() }) {
-                            Icon(
-                                Icons.Filled.ArrowBack,
-                                stringResource(id = R.string.arrow_back),
-                            )
+            Surface {
+                TopAppBar(
+                    colors =
+                        topAppBarColors(
+                            containerColor = MaterialTheme.colorScheme.background,
+                            titleContentColor = MaterialTheme.colorScheme.onBackground,
+                        ),
+                    title = {
+                        MuzzTitleText(value = stringResource(id = R.string.app_name))
+                    },
+                    navigationIcon = {
+                        if (navBackStackEntry?.destination?.route !in noBackButtonScreens) {
+                            // Show Back button
+                            IconButton(onClick = { navController.navigateUp() }) {
+                                Icon(
+                                    Icons.Filled.ArrowBack,
+                                    contentDescription = stringResource(id = R.string.arrow_back),
+                                )
+                            }
                         }
-                    }
-                },
-            )
+                    },
+                )
+            }
         },
     ) {
         NavHost(
@@ -72,7 +68,7 @@ fun MainNavHost() {
             navController = navController,
             startDestination = MainNavItems.Start.route,
         ) {
-            addMain(navController = navController)
+            addMain()
         }
     }
 }
