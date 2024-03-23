5f109787237d453134660b8d8d0eb69a7d39cf27
Gaetano Dati <datiga001@group.pirelli.com>
2024-03-23 20:38:25 +0100

    Improved MuzzMessage.kt and removed useless string for placeholder

--- app/src/main/java/com/garrodroideveloper/muzzexercise/shared/ui/components/MuzzMessage.kt
+++ app/src/main/java/com/garrodroideveloper/muzzexercise/shared/ui/components/MuzzMessage.kt
@@ -9,10 +9,12 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -21,13 +23,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.garrodroideveloper.muzzexercise.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MuzzSendMessageBox(
     onMessageSent: (String) -> Unit,
@@ -36,6 +40,13 @@ fun MuzzSendMessageBox(
     var chatBoxValue by remember { mutableStateOf(TextFieldValue("")) }
     Row(modifier = modifier.padding(dimensionResource(id = R.dimen.single_margin))) {
         TextField(
+            colors =
+                TextFieldDefaults.colors(
+                    disabledTextColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    disabledIndicatorColor = Color.Transparent,
+                ),
             value = chatBoxValue,
             onValueChange = { newText ->
                 chatBoxValue = newText
@@ -45,9 +56,6 @@ fun MuzzSendMessageBox(
                     .weight(1f)
                     .padding(4.dp),
             shape = RoundedCornerShape(24.dp),
-            placeholder = {
-                MuzzSmallText(value = stringResource(id = R.string.type_something))
-            },
         )
         IconButton(
             enabled = chatBoxValue.text.isNotEmpty(),
@@ -60,13 +68,23 @@ fun MuzzSendMessageBox(
             modifier =
                 Modifier
                     .clip(CircleShape)
-                    .background(color = MaterialTheme.colorScheme.primary)
+                    .background(
+                        brush =
+                            Brush.verticalGradient(
+                                colors =
+                                    listOf(
+                                        MaterialTheme.colorScheme.primary,
+                                        MaterialTheme.colorScheme.onPrimary,
+                                    ),
+                            ),
+                    )
                     .align(Alignment.CenterVertically),
         ) {
             Icon(
                 imageVector = Icons.Filled.Send,
                 contentDescription = "Send",
                 modifier = Modifier.fillMaxSize().padding(8.dp),
+                tint = Color.White,
             )
         }
     }
