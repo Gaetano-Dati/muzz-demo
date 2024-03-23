997513a943737db41f9acd4377ae2192bcf1f01e
Gaetano Dati <datiga001@group.pirelli.com>
2024-03-23 19:55:17 +0100

    Designed chat items

--- app/src/main/java/com/garrodroideveloper/muzzexercise/shared/ui/components/MuzzMessage.kt
+++ app/src/main/java/com/garrodroideveloper/muzzexercise/shared/ui/components/MuzzMessage.kt
@@ -1,16 +1,25 @@
 package com.garrodroideveloper.muzzexercise.shared.ui.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
@@ -44,8 +53,62 @@ fun MuzzMessage(onMessageSent: (String) -> Unit) {
     }
 }
 
+@Composable
+fun MuzzMessageOtherItem(text: String) {
+    Box(
+        modifier =
+            Modifier
+                .clip(
+                    RoundedCornerShape(
+                        topStart = 48f,
+                        topEnd = 48f,
+                        bottomStart = 0f,
+                        bottomEnd = 48f,
+                    ),
+                )
+                .background(MaterialTheme.colorScheme.onSecondary)
+                .padding(dimensionResource(id = R.dimen.single_margin)),
+        contentAlignment = Alignment.CenterStart,
+    ) {
+        MuzzSmallText(value = text)
+    }
+}
+
+@Composable
+fun MuzzMessageMineItem(text: String) {
+    Box(
+        modifier =
+            Modifier
+                .clip(
+                    RoundedCornerShape(
+                        topStart = 48f,
+                        topEnd = 48f,
+                        bottomStart = 48f,
+                        bottomEnd = 0f,
+                    ),
+                )
+                .background(MaterialTheme.colorScheme.primary)
+                .padding(dimensionResource(id = R.dimen.single_margin)),
+        contentAlignment = Alignment.CenterStart,
+    ) {
+        MuzzSmallText(value = text)
+    }
+}
+
 @Preview
 @Composable
 private fun MuzzMessagePreview() {
     MuzzMessage({})
+}
+
+@Preview
+@Composable
+private fun MuzzMessageOtherItemPreview() {
+    MuzzMessageOtherItem("Other item")
+}
+
+@Preview
+@Composable
+private fun MuzzMessageMineItemPreview() {
+    MuzzMessageMineItem("Mine item")
 }
